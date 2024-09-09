a258d4a8e18a4b863627708c15f1c9e6e3090bda
Alexcious91 <radiopanetiisetso@gmail.com>
2024-09-09 16:02:00 +0200

    Modified chapter 03

--- chapter_03/src/exercise/ShoppingCart.java
+++ chapter_03/src/exercise/ShoppingCart.java
@@ -2,6 +2,18 @@ package exercise;
 
 public class ShoppingCart {
     public static void main(String[] args) {
-        System.out.println("Welcome to Shopping Cart");
+        System.out.println("Welcome to Shopping!");
+
+        System.out.println(printNums(250));
+    }
+
+
+    public static int printNums(int num){
+        if(num == 25){
+            return 25;
+        }else {
+            return printNums(100);
+
+        }
     }
 }
